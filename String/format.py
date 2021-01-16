0e6e3a51b039a918f0d8dcfa179f4ed0cd912f12
babynicth <nichada.th@ku.th>
2021-01-16 10:37:27 +0700

    add format

--- String/format.py
+++ String/format.py
@@ -3,4 +3,4 @@ txt = "My name is Meaw, and I am {}"
 
 result = txt.format(age)
 
-print("result", result)  # Output: "My name is Meaw, and I am 20"
+print("result:", result)  # Output: "My name is Meaw, and I am 20"
